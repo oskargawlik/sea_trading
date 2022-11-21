32629406c93192e075cc5004ab780c8ae3b01a38
Oskar Gawlik <o.gav13733@gmail.com>
2022-11-21 22:20:57 +0100

    multiple constructors added

--- shipping.py
+++ shipping.py
@@ -1,18 +1,38 @@
+import iso6346
+
+
 class ShippingContainer:
     next_serial = 1337
 
-    @staticmethod
-    def _generate_serial():
-        result = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+    @classmethod
+    def _generate_serial(cls):
+        result = cls.next_serial
+        cls.next_serial += 1
         return result
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6)
+        )
+
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code, contents=[])
+
+    @classmethod
+    def create_with_items(cls, owner_code, items):
+        return cls(owner_code, contents=list(items))
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
 
 
-sc = ShippingContainer(42, 'drugs')
-print(sc.next_serial)
-print(ShippingContainer.next_serial)
+sc = ShippingContainer('ELO', ['drugs'])
+print(sc.bic)
