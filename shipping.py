fc28adddb2d234696a5a4406acd14c2a32c0e006
Oskar Gawlik <o.gav13733@gmail.com>
2022-11-21 21:43:25 +0100

    example of static method

---
+++ shipping.py
@@ -0,0 +1,18 @@
+class ShippingContainer:
+    next_serial = 1337
+
+    @staticmethod
+    def _generate_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer._generate_serial()
+
+
+sc = ShippingContainer(42, 'drugs')
+print(sc.next_serial)
+print(ShippingContainer.next_serial)
