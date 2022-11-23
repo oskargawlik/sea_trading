d7e1e6b5b361ea4fac36fea2d8b94d1cac773d9c
Oskar Gawlik <o.gav13733@gmail.com>
2022-11-23 22:43:01 +0100

    awesome class decorator

--- position.py
+++ position.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class Position:
     def __init__(self, latitude, longitude):
         if not (-90 <= latitude <= 90):
@@ -51,3 +54,64 @@ class EarthPosition(Position):
 class MarsPosition(Position):
     pass
 
+
+def auto_repr(cls):
+    members = vars(cls)
+
+    if '__repr__' in members:
+        raise TypeError(f'{cls.__name__} already defines __repr__')
+
+    if '__init__' not in members:
+        raise TypeError(f'{cls.__name__} does not override __init__')
+
+    signature = inspect.signature(cls.__init__)
+    parameter_names = list(signature.parameters)[1:]
+
+    if not all(
+        isinstance(members.get(name, None), property)
+        for name in parameter_names
+     ):
+        raise TypeError(
+            f'Cannot apply auto_repr to {cls.__name__} '
+            f'because not all __init__ parameters have matching properties')
+
+    def synthesized_repr(self):
+        return "{typename}({args})".format(
+            typename=cls.__name__,
+            args=', '.join(
+                "{name}={value}".format(
+                    name=name,
+                    value=getattr(self, name)
+                ) for name in parameter_names
+            )
+        )
+
+    setattr(cls, "__repr__", synthesized_repr)
+    return cls
+
+
+@auto_repr
+class Location:
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def position(self):
+        return self._position
+
+    def __str__(self):
+        return self.name
+
+
+hong_kong = Location("Hong Kong", EarthPosition(22.29, 114.16))
+stockholm = Location("Stockholm", EarthPosition(59.33, 18.06))
+cape_town = Location("Cape Town", EarthPosition(-33.93, 18.42))
+rotterdam = Location("Rotterdam", EarthPosition(51.96, 4.47))
+maracaibo = Location("Maracaibo", EarthPosition(10.65, -71.65))
+
+print(repr(maracaibo))
