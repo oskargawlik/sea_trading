906796c6e2eeae27f52fd2585c55a9542008ca6e
Oskar Gawlik <o.gav13733@gmail.com>
2022-11-23 21:09:04 +0100

    overriding getters/setters/staitcmethod/classmethod

--- shipping.py
+++ shipping.py
@@ -2,6 +2,9 @@ import iso6346
 
 
 class ShippingContainer:
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
     next_serial = 1337
 
     @classmethod
@@ -18,30 +21,36 @@ class ShippingContainer:
         )
 
     @classmethod
-    def create_empty(cls, owner_code, **kwargs):
-        return cls(owner_code, contents=[], **kwargs)
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents=[], **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items, **kwargs):
-        return cls(owner_code, contents=list(items), **kwargs)
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(items), **kwargs)
 
-    def __init__(self, owner_code, contents, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
         self.bic = type(self)._make_bic_code(
             owner_code=owner_code,
             serial=ShippingContainer._generate_serial()
         )
 
+    def _calc_volume(self):
+        return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
+
+    @property
+    def volume_ft3(self):
+        return self._calc_volume()
+
 
 class RefrigeratedShippingContainer(ShippingContainer):
     MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
 
-    def __init__(self, owner_code, contents, *, celsius, **kwargs):
-        super().__init__(owner_code, contents)
-        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
-            raise ValueError('Temperature too hot')
-
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, length_ft, contents)
         self.celsius = celsius
 
     @property
@@ -50,9 +59,20 @@ class RefrigeratedShippingContainer(ShippingContainer):
 
     @celsius.setter
     def celsius(self, temperature):
+        self._set_celsius(temperature)
+
+    def _set_celsius(self, temperature):
         if temperature > RefrigeratedShippingContainer.MAX_CELSIUS:
-            raise ValueError('Temperature too hot')
+            raise ValueError('Temperature too hot!')
         self._celsius = temperature
+
+    @property
+    def fahrenheit(self):
+        return self.celsius * 9 / 5 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = (value - 32) * 5 / 9
 
     @staticmethod
     def _make_bic_code(owner_code, serial):
@@ -62,8 +82,24 @@ class RefrigeratedShippingContainer(ShippingContainer):
             category='R'
         )
 
+    def _calc_volume(self):
+        return super()._calc_volume() - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
 
-sc = ShippingContainer('ELO', ['drugs'])
-rsc = RefrigeratedShippingContainer('ELO', ['drugs'], celsius=4)
-rsc2 = RefrigeratedShippingContainer.create_empty('ELO', celsius=3)
-print(sc.bic)
+
+class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
+    MIN_CELSIUS = -20
+
+    def _set_celsius(self, value):
+        if HeatedRefrigeratedShippingContainer.MIN_CELSIUS > value:
+            raise ValueError(f'Temperature too cold!')
+        super()._set_celsius(value)
+
+
+sc = ShippingContainer('elo', 200, ['drugs'])
+rsc = RefrigeratedShippingContainer('ELO', 200, ['drugs'], celsius=3)
+hrsc = HeatedRefrigeratedShippingContainer('ELO', 200, ['drugs'], celsius=3)
+
+print(sc.volume_ft3)
+print(rsc.volume_ft3)
+hrsc.celsius = 2
+print(hrsc.celsius)
